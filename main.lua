558f40b1bd93f3f1d120e7c72328badeca904282
Ashley Fisher <fish.ash@gmail.com>
2012-10-07 23:29:12 -0700

    Drew the puzzle on screen. It's ugly.

--- main.lua
+++ main.lua
@@ -1,3 +1,37 @@
+require "parse"
+
+local puzzle
+local metadata
+
+function love.load()
+    puzzle = parse.loadPuzzle("puzzles/test.txt")
+    metadata = puzzle.title .. " by " .. puzzle.author
+
+    love.graphics.setCaption(metadata)
+end
+
 function love.draw()
-    love.graphics.print("Picross", 200, 300)
+    local x, y = 0, 0
+    local size = 50
+
+    for r = 1, puzzle.size.rows do
+        for c = 1, puzzle.size.cols do
+            if puzzle.get(r, c) == "#" then
+                love.graphics.rectangle("fill", x, y, size, size)
+            else
+                love.graphics.rectangle("line", x, y, size, size)
+            end
+            x = x + size
+        end
+        y = y + size
+        x = 0
+    end
+
+    love.graphics.print(metadata, x, y)
+end
+
+function love.keypressed(key, unicode)
+    if key == "escape" or key == "q" then
+        love.event.quit()
+    end
 end
